2e30391ec9f6183420c84fbdecc4f30104fa730f
prin <hi@prin.studio>
2023-09-23 00:28:34 +0800

    feat: app options manager

---
+++ src/core/storage/index.ts
@@ -0,0 +1,14 @@
+import { DatabaseManager } from './database';
+import { AppOptionsManager } from './options';
+
+/**
+ * Global options manager singleton instance.
+ */
+const appOptionsManager = new AppOptionsManager();
+
+/**
+ * Global database manager singleton instance.
+ */
+const databaseManager = new DatabaseManager();
+
+export { appOptionsManager as options, databaseManager as db };
---
+++ src/core/storage/options.ts
@@ -0,0 +1,77 @@
+import { Signal } from '@preact/signals';
+import { isEqual, safeJSONParse } from '@/utils';
+import logger from '@/utils/logger';
+import packageJson from '@/../package.json';
+
+/**
+ * Type for global app options.
+ */
+export interface AppOptions {
+  theme?: string;
+  disabledExtensions?: string[];
+  version?: string;
+}
+
+const LOCAL_STORAGE_KEY = packageJson.name;
+
+/**
+ * Persist app options to browser local storage.
+ */
+export class AppOptionsManager {
+  private appOptions: AppOptions = {};
+  private previous: AppOptions = {};
+
+  /**
+   * Signal for subscribing to option changes.
+   */
+  public signal = new Signal(0);
+
+  constructor() {
+    this.loadAppOptions();
+  }
+
+  public get<T extends keyof AppOptions>(key: T, defaultValue?: AppOptions[T]) {
+    return this.appOptions[key] ?? defaultValue;
+  }
+
+  public set<T extends keyof AppOptions>(key: T, value: AppOptions[T]) {
+    this.appOptions[key] = value;
+    this.saveAppOptions();
+  }
+
+  /**
+   * Read app options from local storage.
+   */
+  private loadAppOptions() {
+    this.appOptions = {
+      ...this.appOptions,
+      ...safeJSONParse(localStorage.getItem(LOCAL_STORAGE_KEY) || '{}'),
+    };
+
+    this.previous = { ...this.appOptions };
+    logger.info('App options loaded.', this.appOptions);
+    this.signal.value++;
+  }
+
+  /**
+   * Write app options to local storage.
+   */
+  private saveAppOptions() {
+    const oldValue = this.previous;
+    const newValue = {
+      ...this.appOptions,
+      version: packageJson.version,
+    };
+
+    if (isEqual(oldValue, newValue)) {
+      return;
+    }
+
+    this.appOptions = newValue;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.appOptions));
+
+    this.previous = { ...this.appOptions };
+    logger.info('App options saved.', this.appOptions);
+    this.signal.value++;
+  }
+}
