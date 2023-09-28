276ef793535315ef00ad729170b7210089f85a86
prin <hi@prin.studio>
2023-09-28 18:48:38 +0800

    feat: add many columns for user table

--- src/components/user-table.tsx
+++ src/components/user-table.tsx
@@ -56,9 +56,40 @@ const columns = [
       </div>
     ),
   }),
+  columnHelper.accessor('legacy.profile_banner_url', {
+    header: () => <span>Profile Banner</span>,
+    cell: (info) => (
+      <div
+        class="cursor-pointer w-36 h-12"
+        onClick={() => (mediaPreviewSignal.value = info.getValue())}
+      >
+        <img class="w-auto h-12 rounded" src={`${info.getValue()}/600x200`} />
+      </div>
+    ),
+  }),
   columnHelper.accessor('legacy.followers_count', {
     header: () => <span>Followers</span>,
     cell: (info) => <p>{info.getValue()}</p>,
+  }),
+  columnHelper.accessor('legacy.statuses_count', {
+    header: () => <span>Statuses</span>,
+    cell: (info) => <p>{info.getValue()}</p>,
+  }),
+  columnHelper.accessor('legacy.favourites_count', {
+    header: () => <span>Favourites</span>,
+    cell: (info) => <p>{info.getValue()}</p>,
+  }),
+  columnHelper.accessor('legacy.listed_count', {
+    header: () => <span>Listed</span>,
+    cell: (info) => <p>{info.getValue()}</p>,
+  }),
+  columnHelper.accessor('legacy.verified_type', {
+    header: () => <span>Verified Type</span>,
+    cell: (info) => <p>{info.getValue() ?? 'N/A'}</p>,
+  }),
+  columnHelper.accessor('is_blue_verified', {
+    header: () => <span>Blue Verified</span>,
+    cell: (info) => <p>{info.getValue() ? 'YES' : 'NO'}</p>,
   }),
   columnHelper.accessor('legacy.following', {
     header: () => <span>Following</span>,
@@ -67,6 +98,10 @@ const columns = [
   columnHelper.accessor('legacy.followed_by', {
     header: () => <span>Follows You</span>,
     cell: (info) => <p>{info.getValue() ? 'YES' : 'NO'}</p>,
+  }),
+  columnHelper.accessor('legacy.created_at', {
+    header: () => <span>Created At</span>,
+    cell: (info) => <p class="w-32 text-xs">{info.getValue()}</p>,
   }),
 ];
 
